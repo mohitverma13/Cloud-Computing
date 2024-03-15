65f358e5005851ee74b397ce97372f097a55bedd
Mohit Kumar <119738953+mohitverma13@users.noreply.github.com>
2024-03-15 11:57:37 +0530

    Update exp1.4.py

--- exp1.4.py
+++ exp1.4.py
@@ -1,1 +1,26 @@
 print("hello world")
+def cloud_computing_graphic():
+    graphic = """
+        .-~~~-.
+  .-~~~'  _  '~---.
+ /  .-~ ~~     .'   \_
+(~~~____~)  _.'______~~)
+ \~~\  ~~.-'          /
+  \  \               /
+   )  )  (         _/
+  /  /   /c-~~_.-~~
+ ( _/   (  (1 1(
+   ~-._ _.~' 1 1 1 1|
+     \    ~/1  1 1 |
+     ( ~~(  1 1 1 1|
+     \__/~--1 1 1 1|
+          \__\_1 1 1|
+           |(   1 |~~
+           |  \  |~~~
+           |___\-'
+    """
+    print(graphic)
+
+# Call the function to display the graphic
+cloud_computing_graphic()
+
